0b853c5e8f8216387f5c5bcd6701b8755fd363a8
gokulnath <us.gokulnath.com>
2023-08-29 17:55:26 +0530

    first commit

---
+++ Views/MainScreenStyles.js
@@ -0,0 +1,82 @@
+module.exports ={
+    main_screen:{
+        marginTop:'30%',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        width:'100%',
+        height: '100%',
+    },
+    main_screen_display:{
+        elevation:10,
+        width: '95%',
+        backgroundColor:'white',
+        borderRadius:10,
+        display:'flex',
+        marginBottom:10,
+        padding:10
+    },
+    main_screen_display_text:{
+        fontSize:50,
+        textAlign: 'right',
+    },
+    main_screen__keypad:{
+        width:'100%',
+        height: '70%',
+        display:'flex',
+        // flexDirection:'row',
+    },
+    main_screen__keypad_row:{
+        display:'flex',
+        flexDirection:'row',
+        backgroundColor:'white',
+        width:'100%',
+        justifyContent:'space-between',
+        alignItems:'center',
+        padding:10,
+    },
+    btn1_outer:{
+        width:90,
+        height:90,
+        backgroundColor:'white',
+        elevation:2,
+        overflow:'hidden',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:90
+    },
+    bg_button:{
+        backgroundColor:'white',
+        color:'black',
+        fontSize:35,
+        borderRadius:20,
+    },
+    btn_outer:{
+        width:90,
+        height:90,
+        backgroundColor:'pink',
+        borderRadius:90,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    bg1_button:{
+        backgroundColor:'pink',
+        color:'black',
+        fontSize:30,
+        fontWeight:'bold'
+    },
+    btn2_outer:{
+        width:90,
+        height:90,
+        backgroundColor:'grey',
+        borderRadius:90,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    bg2_buton:{
+       
+        color:'white',
+        fontSize:30,
+        fontWeight:'bold'
+    },
+}
